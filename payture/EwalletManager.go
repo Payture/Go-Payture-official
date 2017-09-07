7b602ce5533f37b3e3c6067e003e87e11db88433
fiery-spirit666 <essyport@gmail.com>
2017-09-07 16:48:36 +0300

    refactoring stage_2; add some docs (missed files)

--- payture/EwalletManager.go
+++ payture/EwalletManager.go
@@ -7,14 +7,13 @@ import (
 	"net/http"
 )
 
-/*///////////
-EWallet types
-*/
+//EwalletManager type provide the access for calling EWallet Service API in Payture system.
 type EwalletManager struct {
 	Customers map[string]Customer
 	OrderManager
 }
 
+//EwalletService method that returns the EwalletManager.
 func EwalletService(merch Merchant) (ew EwalletManager) {
 	ew.apiType = "vwapi"
 	ew.merchant = merch
@@ -22,6 +21,7 @@ func EwalletService(merch Merchant) (ew EwalletManager) {
 	return
 }
 
+//NotRegisteredCard type that represent information about card that you must provide for registration in Payture system.
 type NotRegisteredCard struct {
 	CardHolder string
 	EMonth     string
@@ -30,6 +30,7 @@ type NotRegisteredCard struct {
 	CardNumber string
 }
 
+//EWalletCard type that represent information about card that you recieved then call GetCardList method.
 type EWalletCard struct {
 	CardName   string `xml:"CardName,attr"`
 	CardId     string `xml:"CardId,attr"`
@@ -39,6 +40,7 @@ type EWalletCard struct {
 	NoCVV      bool   `xml:"NoCVV,attr"`
 }
 
+//Customer type that represent information about customer.
 type Customer struct {
 	VWUserLgn   string
 	VWUserPsw   string
@@ -57,16 +59,7 @@ func (card NotRegisteredCard) plain() string {
 	return fmt.Sprintf("CardHolder=%s;EMonth=%s;EYear=%s;SecureCode=%s;CardNumber=%s;", card.CardHolder, card.EMonth, card.EYear, card.SecureCode, card.CardNumber)
 }
 
-/*
-EWallet types
-*/ ///////////
-
-/////////////////////////
-/////////////////////////
-
-/*////////////////////////
-Manage Customer collection
-*/
+//AddCustomerToCollection added customer to the EWalletManager's customer collection (but not in the Payture system).
 func (this EwalletManager) AddCustomerToCollection(cust Customer) (EwalletManager, bool) {
 	if this.Customers == nil {
 		this.Customers = make(map[string]Customer)
@@ -81,6 +74,7 @@ func (this EwalletManager) AddCustomerToCollection(cust Customer) (EwalletManage
 	return this, false
 }
 
+//RemoveCustomerFromCollection removed customer from the EWalletManager's customer collection (but not from the Payture system).
 func (this EwalletManager) RemoveCustomerFromCollection(custLogin string) (EwalletManager, bool) {
 	if this.Customers == nil {
 		return this, false
@@ -94,108 +88,91 @@ func (this EwalletManager) RemoveCustomerFromCollection(custLogin string) (Ewall
 	return this, false
 }
 
-/*
-Manage Customer collection
-*/ ////////////////////////
-
-/////////////////////////
-/////////////////////////
-
-/*///////////
-Transactions
-*/
+//InitTransaction create and returned Session in Payture system by which the customer can make a payment or can registered card.
 func (this EwalletManager) InitTransaction(custLogin string, sessionType string, ip string, order Payment, tag string, lang string, cardId string) (initResp InitResponse, err error) {
 	cust := this.Customers[custLogin]
-	err = this.sendEWRequestMap(&initResp, "Init", cust.plain()+order.plain()+fmt.Sprintf("IP=%s;TemplateTag=%s;Language=%s;CardId=%s;SessionType=%s;CustomField=Field;", ip,
+	err = this.sendEWRequestMap(&initResp, INIT, cust.plain()+order.plain()+fmt.Sprintf("IP=%s;TemplateTag=%s;Language=%s;CardId=%s;SessionType=%s;CustomField=Field;", ip,
 		tag, lang, cardId, sessionType /*add mooooooreee*/))
 	return
 }
 
+//PayRegCard makes a payment or blocks funds on customer's card. Required information about card that was attached to specified customer.
 func (this EwalletManager) PayRegCard(custLogin string, cardId string, secureCode string, order Payment, ip string, confirmCode string, custFields map[string]string) (payResp EWalletPayResponse, err error) {
 	cust := this.Customers[custLogin]
-	err = this.sendEWRequestMap(&payResp, "Pay", cust.plain()+order.plain()+fmt.Sprintf("IP=%s;ConfirmCode=%s;CardId=%s;SecureCode=%s;CustomField=Field;", ip, confirmCode, cardId, secureCode /*add mooooooreee*/))
+	err = this.sendEWRequestMap(&payResp, PAY, cust.plain()+order.plain()+fmt.Sprintf("IP=%s;ConfirmCode=%s;CardId=%s;SecureCode=%s;CustomField=Field;", ip, confirmCode, cardId, secureCode /*add mooooooreee*/))
 	return
 }
 
+//PayNoRegCard makes a payment or blocks funds on customer's card. Required full information about card that wasn't attached to customer.
 func (this EwalletManager) PayNoRegCard(custLogin string, card NotRegisteredCard, order Payment, ip string, confirmCode string, custFields map[string]string, addCardToCust bool) (payResp EWalletPayResponse, err error) {
 	cust := this.Customers[custLogin]
-	err = this.sendEWRequestMap(&payResp, "Pay", cust.plain()+card.plain()+order.plain()+fmt.Sprintf("IP=%s;ConfirmCode=%s;CardId=FreePay;CustomField=Field;", ip, confirmCode /*add mooooooreee*/))
+	err = this.sendEWRequestMap(&payResp, PAY, cust.plain()+card.plain()+order.plain()+fmt.Sprintf("IP=%s;ConfirmCode=%s;CardId=FreePay;CustomField=Field;", ip, confirmCode /*add mooooooreee*/))
 	return
 }
 
+//OperationBySession makes a payment, blocks funds on customer's card or registers card in Payture system by SessionId.
 func (this EwalletManager) OperationBySession(cmd string, sessionId string) (*http.Response, error) { //For Add or Pay operation on Payture side
-	params := map[string][]string{
-		"SessionId": []string{sessionId}}
-	return sendRequest(this, cmd, params)
+	prms := reqPrms{}.set(SESSIONID, sessionId).get()
+	return sendRequest(this, cmd, prms)
 }
 
+//SendCode to customer in the process of payment for secure reasons.
 func (this EwalletManager) SendCode(custLogin string) (*http.Response, error) {
 	cust := this.Customers[custLogin]
-	return this.sendEWRequest("SendCode", cust.plain())
+	return this.sendEWRequest(SENDCODE, cust.plain())
 }
 
-/*
-Transactions
-*/ //////////
-
-/////////////////////////
-/////////////////////////
-
-/*//////////
-Manage Card
-*/
-
+//CardAdd method for add supplied card to specified customer.
 func (this EwalletManager) CardAdd(custLogin string, card NotRegisteredCard) (cardResp CardResponse, err error) {
 	cust := this.Customers[custLogin]
-	err = this.sendEWRequestMap(&cardResp, "Add", cust.plain()+card.plain())
+	err = this.sendEWRequestMap(&cardResp, ADD, cust.plain()+card.plain())
 	return
 }
 
+//CardActivate method for activate card that was attached to specified customer.
 func (this EwalletManager) CardActivate(custLogin string, cardId string, amount string) (cardResp CardResponse, err error) {
 	cust := this.Customers[custLogin]
-	err = this.sendEWRequestMap(&cardResp, "Activate", cust.plain()+fmt.Sprintf("CardId=%s;Amount=%s;", cardId, amount))
+	err = this.sendEWRequestMap(&cardResp, ACTIVATE, cust.plain()+fmt.Sprintf("CardId=%s;Amount=%s;", cardId, amount))
 	return
 }
 
+//CardRemove method for remove card with specified CardId from cutomer cards list.
 func (this EwalletManager) CardRemove(custLogin string, cardId string) (cardResp CardResponse, err error) {
 	cust := this.Customers[custLogin]
-	err = this.sendEWRequestMap(&cardResp, "Remove", cust.plain()+fmt.Sprintf("CardId=%s;", cardId))
+	err = this.sendEWRequestMap(&cardResp, REMOVE, cust.plain()+fmt.Sprintf("CardId=%s;", cardId))
 	return
 }
 
-/*
-Manage Card
-*/ /////////
-
-/////////////////////////
-/////////////////////////
-
-/*/////////////
-Manage Customer
-*/
-
+//CustomerRegister method registers supplied customer in Payture system.
 func (this EwalletManager) CustomerRegister(cust Customer) (custResp CustomerResponse, err error) {
-	return this.customRequest(cust, "Register")
+	return this.customRequest(cust, REGISTER)
 }
 
+//CustomerDelete delete specified customer from Payture system.
 func (this EwalletManager) CustomerDelete(custLogin string) (custResp CustomerResponse, err error) {
-	return this.customRequest(this.Customers[custLogin], "Delete")
+	return this.customRequest(this.Customers[custLogin], DELETE)
 }
 
+//CustomerUpdate changed some information in customer's account in Payture system.
 func (this EwalletManager) CustomerUpdate(cust Customer) (custResp CustomerResponse, err error) {
-	return this.customRequest(cust, "Update")
+	return this.customRequest(cust, UPDATE)
 }
+
+//CustomerCheckBylogin check is the customer was registered in Payture system by login.
+//The Customer must be in the customer's collection at the time when the method was called.
 func (this EwalletManager) CustomerCheckBylogin(custLogin string) (custResp CustomerResponse, err error) {
-	return this.customRequest(this.Customers[custLogin], "Check")
+	return this.customRequest(this.Customers[custLogin], CHECK)
 }
 
+//CustomerCheck check is the customer was registered in Payture system.
 func (this EwalletManager) CustomerCheck(cust Customer, addToCollection bool) (custResp CustomerResponse, err error) {
-	return this.customRequest(cust, "Check")
+	return this.customRequest(cust, CHECK)
 }
 
+//GetCardList returned the card's collection that was attached to specified customer.
 func (this EwalletManager) GetCardList(custLogin string) (*http.Response, error) {
 	cust := this.Customers[custLogin]
-	return this.sendEWRequest("GetList", cust.plain())
+	return this.sendEWRequest(GETLIST, cust.plain())
 }
 
 func (cust *Customer) FillCardList(resp *http.Response) {
@@ -207,19 +184,9 @@ func (cust *Customer) FillCardList(resp *http.Response) {
 	fmt.Println(err)
 }
 
-/*
-Manage Customer
-*/ ////////////
-
-/*//////////////////
-Send Ewallet Request
-*/
-
 func (this EwalletManager) sendEWRequest(cmd string, data string) (*http.Response, error) {
-	params := map[string][]string{
-		"VWID": []string{this.merchant.Key},
-		"DATA": []string{data}}
-	return sendRequest(this, cmd, params)
+	par := reqPrms{}.set(VWID, this.merchant.Key).set(DATAUP, data)
+	return sendRequest(this, cmd, par.get())
 }
 
 func (this EwalletManager) customRequest(cust Customer, cmd string) (custResp CustomerResponse, err error) {
@@ -228,20 +195,10 @@ func (this EwalletManager) customRequest(cust Customer, cmd string) (custResp Cu
 }
 
 func (this EwalletManager) sendEWRequestMap(ret Unwrapper, cmd string, data string) (err error) {
-	params := map[string][]string{
-		"VWID": []string{this.merchant.Key},
-		"DATA": []string{data}}
-	err = sendRequestAndMap(ret, params, this, cmd)
+	prms := reqPrms{}.set(VWID, this.merchant.Key).set(DATAUP, data)
+	err = sendRequestAndMap(ret, prms.get(), this, cmd)
 	return
 }
-
-/*
-Send Ewallet Request
-*/ //////////////////
-
-/*///////
-Responses
-*/
 
 type EWalletPayResponse struct {
 	OrderId string `xml:"OrderId,attr"`
